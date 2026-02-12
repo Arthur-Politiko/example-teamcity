ff8e4ee517e80bffbe0c0ab8a1099d416d768ca7
paz <paz@TeamCity>
2026-02-12 16:29:16 +0000

    Synchronization with own VCS root is enabled (TeamCity change in 'ntlg' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,93 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2025.11"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "diag"
+            id = "diag"
+            enabled = false
+            scriptContent = """
+                echo "=== Проверка settings.xml ==="
+                ls -la ~/.m2/
+                cat ~/.m2/settings.xml 2>/dev/null || echo "settings.xml не найден"
+                
+                echo "=== Проверка переменных ==="
+                env | grep MAVEN
+                env | grep M2
+            """.trimIndent()
+        }
+        maven {
+            name = "clean deploy"
+            id = "Maven2"
+
+            conditions {
+                contains("teamcity.build.branch", "master")
+            }
+            goals = "clean deploy"
+            runnerArgs = "-X -e -Dmaven.test.failure.ignore=true"
+            userSettingsSelection = "settings.xml"
+        }
+        maven {
+            name = "clean test"
+            id = "clean_test"
+            enabled = false
+
+            conditions {
+                doesNotContain("teamcity.build.branch", "master")
+            }
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        maven {
+            name = "deploy"
+            id = "deploy"
+            enabled = false
+            goals = "mvn -s /path/to/settings.xml -X deploy"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            userSettingsSelection = "settings.xml"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})

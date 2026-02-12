a7ef38a88cf0cc7ffb6aef8eaa7a9f65f0591c11
paz <paz@TeamCity>
2026-02-12 18:10:08 +0000

    general settings of 'Build' build configuration were updated (TeamCity change in 'ntlg' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,6 +35,9 @@ project {
 object Build : BuildType({
     name = "Build"
 
+    artifactRules = "target/*.jar"
+    publishArtifacts = PublishMode.SUCCESSFUL
+
     vcs {
         root(DslContext.settingsRoot)
     }
